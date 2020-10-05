ac9c92a8ae20943075b6bc8f6b09702c70e78a83
Kayzad Kararia <kayzadkararia@gmail.com>
2020-10-05 12:08:33 +0530

    my portfolio website

---
+++ templates/race.py
@@ -0,0 +1,16 @@
+import time
+import turtle as t
+from random import randint
+from turtle import Turtle 
+
+
+window=t.screen()
+window.title("TURTLE RACE")
+
+t.bgcolor("forestgreen")
+t.color('blue')
+t.speed(0)
+t.penup()
+t.setpos(-140,200)
+t.write("TURTLE RACE", font = {"Arial", 30, "bold"} )
+t.penup()
